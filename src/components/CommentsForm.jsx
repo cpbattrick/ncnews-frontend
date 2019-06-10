ad7aaae7dcfa30f67204070c0c4a3371e2000e9d
cpbattrick <cpbattrick@googlemail.com>
2019-06-10 08:32:21 -0400

    updated commentsform to use material ui components and added styling

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 import { postComment } from "../api";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const styles = theme => ({
+  card: {
+    minWidth: 275,
+    width: "90%",
+    marginTop: "20px",
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  submitButton: {}
+});
 
 class CommentForm extends React.Component {
   state = {
@@ -26,16 +44,30 @@ class CommentForm extends React.Component {
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          Post Comment:{" "}
-          <input required={true} onChange={this.storeUserInput} type="text" />
-          <button type="submit">Press for internet carnage!</button>
-        </form>
-      </div>
+      <Card className={classes.card}>
+        <CardContent>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Post Comment"
+              multiline
+              rowsMax="4"
+              onChange={this.storeUserInput}
+              margin="normal"
+              variant="outlined"
+              required={true}
+            />
+            <div className="submitButton">
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     );
   }
 }
 
-export default CommentForm;
+export default withStyles(styles)(CommentForm);
