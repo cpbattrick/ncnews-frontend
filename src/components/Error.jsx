f9131dbb15fc64495b950ba0478f358a3468eb89
cpbattrick <cpbattrick@googlemail.com>
2019-06-12 16:20:44 -0400

    added typography to error and styling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  error: {
+    fontSize: "30px",
+    textAlign: "center",
+    margin: "1em"
+  }
+}));
 
 const Error = props => {
-  if (props.err) return <div className="error">{props.err}</div>;
+  const classes = useStyles();
+  if (props.err)
+    return <Typography className={classes.error}>{props.err}</Typography>;
   return <div className="error">Route Not Found</div>;
 };
 
